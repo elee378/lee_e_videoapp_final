9b3ef2d87e2d9238d2b1ea4f8b77bd2c4286cbf4
elee378 <ericjaelee@hotmail.com>
2018-04-20 19:39:48 -0400

    initial commit

---
+++ movies.sql
@@ -0,0 +1,440 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 20, 2018 at 11:37 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `movies`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_arating`
+--
+
+CREATE TABLE `tbl_arating` (
+  `arating_id` smallint(5) UNSIGNED NOT NULL,
+  `arating_name` varchar(125) NOT NULL,
+  `arating_desc` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `tbl_arating`
+--
+
+INSERT INTO `tbl_arating` (`arating_id`, `arating_name`, `arating_desc`) VALUES
+(1, 'G', 'G – General Audiences\r\nAll ages admitted. Nothing that would offend parents for viewing by children. '),
+(2, 'PG', 'PG – Parental Guidance Suggested\r\nSome material may not be suitable for children. Parents urged to give  	&ldquo;parental guidance&rdquo;. '),
+(3, 'PG-13', 'PG-13 – Parents Strongly Cautioned\r\nSome material may be inappropriate for children under 13. Parents are urged to be cautious. Some material may be inappropriate for pre-teenagers.'),
+(4, 'R', 'R – Restricted\r\nUnder 17 requires accompanying parent or adult guardian. Contains some adult material. Parents are urged to learn more about the film before taking their young children with them. '),
+(5, 'NC-17', 'NC-17 – Adults Only\r\nNo One 17 and Under Admitted. Clearly adult. Children are not admitted. ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_comments`
+--
+
+CREATE TABLE `tbl_comments` (
+  `comments_id` mediumint(8) UNSIGNED NOT NULL,
+  `comments_auth` varchar(125) DEFAULT NULL,
+  `comments_copy` text NOT NULL,
+  `comments_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comments_movie` int(11) NOT NULL,
+  `comments_rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_comments`
+--
+
+INSERT INTO `tbl_comments` (`comments_id`, `comments_auth`, `comments_copy`, `comments_date`, `comments_movie`, `comments_rating`) VALUES
+(1, NULL, 'Loved it. Chris Pratt is my hero! I LOVE YOU STAR LORD!!', '2018-03-26 19:08:41', 1, 5),
+(2, NULL, 'Not quite as good as the first Guardians, but still WAY better that any DC movie... ', '2018-03-26 19:09:57', 1, 4),
+(3, NULL, 'It sucked.', '2018-04-01 14:54:32', 1, 1),
+(4, NULL, 'It was amazing. I really liked this way better than any DC movie that\'s ever been made, EVER.\n\nCuz DC movies are garbage.', '2018-04-01 14:55:29', 1, 3),
+(5, NULL, 'I thought Logan was great.THIS is the way you do comic book dystopia.', '2018-04-01 14:56:55', 3, 4),
+(6, NULL, 'Way too violent. I thought Hugh Jackman would at least do a song and dance, but was VERY disappointed.', '2018-04-01 14:57:35', 3, 1),
+(7, NULL, 'OMG i can\'t get enough Deadpool.. what a great movie', '2018-04-01 15:11:30', 7, 5),
+(8, NULL, 'I really liked this. Prof X made me sad tho. What a way to go...', '2018-04-01 15:51:58', 3, 4),
+(9, NULL, 'this is my review ', '2018-04-13 15:23:02', 7, 1),
+(10, NULL, 'another review', '2018-04-13 22:27:48', 7, 3),
+(11, NULL, 'another review', '2018-04-13 22:30:33', 7, 3),
+(12, NULL, '1 more', '2018-04-13 22:30:39', 7, 3),
+(13, NULL, 'anjotehr one', '2018-04-13 22:31:02', 7, 3),
+(14, NULL, 'again', '2018-04-13 22:31:06', 7, 3),
+(15, NULL, 'one more', '2018-04-13 22:31:29', 7, 3),
+(16, NULL, 'one more review', '2018-04-13 22:34:24', 7, 3),
+(17, NULL, 'again', '2018-04-13 22:34:34', 7, 3),
+(18, NULL, 'one more', '2018-04-13 22:35:08', 7, 3),
+(19, NULL, 'again ', '2018-04-13 22:35:13', 7, 3),
+(20, NULL, 'more comments ', '2018-04-13 22:35:34', 7, 3),
+(21, NULL, 'another comment for deadpool', '2018-04-13 22:36:56', 7, 3),
+(22, NULL, 'one more comment', '2018-04-13 22:37:20', 7, 3),
+(23, NULL, 'Deadpool was a violent movie :o', '2018-04-13 22:42:42', 7, 3),
+(24, NULL, '', '2018-04-13 22:50:02', 7, 3),
+(25, NULL, 'heres a comment', '2018-04-13 22:51:33', 8, 3),
+(26, NULL, 'heres another comment', '2018-04-13 22:51:52', 8, 3),
+(27, NULL, '', '2018-04-17 15:22:42', 7, 3),
+(28, NULL, '', '2018-04-17 15:22:43', 7, 3),
+(29, NULL, 'my review for trolllllssssssss', '2018-04-17 15:55:10', 10, 3),
+(30, NULL, 'this is a comment ', '2018-04-20 23:12:09', 5, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_director`
+--
+
+CREATE TABLE `tbl_director` (
+  `director_id` smallint(5) UNSIGNED NOT NULL,
+  `director_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_director`
+--
+
+INSERT INTO `tbl_director` (`director_id`, `director_name`) VALUES
+(1, 'James Gunn'),
+(2, 'Christopher Nolan'),
+(3, 'James Mangold'),
+(4, 'Joon-ho Bong'),
+(5, 'Bill Condon'),
+(6, 'Mel Gibson'),
+(7, 'Tim Miller'),
+(8, 'Denis Villeneuve '),
+(9, 'Dexter Fletcher'),
+(10, 'Mike Mitchell'),
+(11, 'Walt Dohrn'),
+(12, 'Alejandro Gonzalez Inarritu'),
+(13, 'John Crowley '),
+(14, 'Robert Zemeckis'),
+(15, 'Florian Gallenberger '),
+(16, 'J.J. Abrams'),
+(17, 'Damien Chazelle'),
+(18, 'Joseph Gordon-Levitt '),
+(19, 'Alfonso Cuaron'),
+(20, 'Ang Lee'),
+(21, 'David O. Russell');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_genre`
+--
+
+CREATE TABLE `tbl_genre` (
+  `genre_id` tinyint(3) UNSIGNED NOT NULL,
+  `genre_name` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_genre`
+--
+
+INSERT INTO `tbl_genre` (`genre_id`, `genre_name`) VALUES
+(1, 'Action'),
+(2, 'Adventure'),
+(3, 'Comedy'),
+(4, 'Crime'),
+(5, 'Drama'),
+(6, 'Historical'),
+(7, 'Horror'),
+(8, 'Musical'),
+(9, 'Science Fiction'),
+(10, 'War'),
+(11, 'Western'),
+(12, 'Animation'),
+(13, 'Family'),
+(14, 'Fantasy'),
+(15, 'Romance'),
+(16, 'Sport');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_movies`
+--
+
+CREATE TABLE `tbl_movies` (
+  `movies_id` mediumint(8) UNSIGNED NOT NULL,
+  `movies_cover` varchar(75) NOT NULL DEFAULT 'cover_default.jpg',
+  `movies_title` varchar(125) NOT NULL,
+  `movies_year` varchar(5) NOT NULL,
+  `movies_runtime` varchar(25) NOT NULL,
+  `movies_storyline` text NOT NULL,
+  `movies_trailer` varchar(75) NOT NULL DEFAULT 'trailer_default.jpg',
+  `movies_release` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_movies`
+--
+
+INSERT INTO `tbl_movies` (`movies_id`, `movies_cover`, `movies_title`, `movies_year`, `movies_runtime`, `movies_storyline`, `movies_trailer`, `movies_release`) VALUES
+(1, 'guardians2.jpg', 'Guardians of the Galaxy Vol. 2', '2017', '2h 16m', 'The Guardians must fight to keep their newfound family together as they unravel the mystery of Peter Quill&rsquo;s true parentage.', 'Guardians2.mp4', 'May 5, 2017'),
+(2, 'dunkirk.jpg', 'Dunkirk', '2017', '1h 46m', 'Allied soldiers from Belgium, the British Empire and France are surrounded by the German army and evacuated during a fierce battle in World War II.', 'Dunkirk.mp4', 'July 21, 2017'),
+(3, 'logan.jpg', 'Logan', '2017', '2h 17m', 'In the near future, a weary Logan cares for an ailing Professor X, somewhere on the Mexican border. However, Logan&rsquo;s attempts to hide from the world, and his legacy, are upended when a young mutant arrives, pursued by dark forces.', 'Logan.mp4', 'March 3,2017'),
+(4, 'okja.jpg', 'Okja', '2017', '2h', ' Meet Mija, a young girl who risks everything to prevent a powerful, multi-national company from kidnapping her best friend - a fascinating animal named Okja. ', 'Okja.mp4', 'June 28,2017'),
+(5, 'beauty.jpg', 'Beauty and the Beast', '2017', '2h 9min', 'An adaptation of the fairy tale about a monstrous-looking prince and a young woman who fall in love. ', 'Beauty.mp4', 'March 17, 2017'),
+(6, 'hacksaw.jpg', 'Hacksaw Ridge', '2016', '2h 19m', 'WWII American Army Medic Desmond T. Doss, who served during the Battle of Okinawa, refuses to kill people, and becomes the first man in American history to receive the Medal of Honor without firing a shot.', 'Hacksaw.mp4', 'November 4, 2016'),
+(7, 'deadpool.jpg', 'Deadpool', '2016', '1h 48m', 'A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge.', 'Deadpool.mp4', 'Febuary 12, 2016'),
+(8, 'arrival.jpg', 'Arrival', '2016', '1h 56m', 'When twelve mysterious spacecraft appear around the world, linguistics professor Louise Banks is tasked with interpreting the language of the apparent alien visitors.', 'Arrival.mp4', 'November 11, 2016'),
+(9, 'eddie.jpg', 'Eddie the Eagle', '2016', '1h 46m', 'The story of Eddie Edwards, the notoriously tenacious British underdog ski jumper who charmed the world at the 1988 Winter Olympics.', 'Eddie.mp4', 'Febuary 26, 2016'),
+(10, 'trolls.jpg', 'Trolls', '2016', '1h 32m', 'After the Bergens invade Troll Village, Poppy, the happiest Troll ever born, and the curmudgeonly Branch set off on a journey to rescue her friends.', 'Trolls.mp4', 'November 4, 2016'),
+(11, 'revenant.jpg', 'The Revenant', '2015', '2h 36m', 'A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team.', 'Revenant.mp4', 'January 8, 2015'),
+(12, 'brooklyn.jpg', 'Brooklyn', '2015', '1h 57m', 'An Irish immigrant lands in 1950s Brooklyn, where she quickly falls into a romance with a local. When her past catches up with her, however, she must choose between two countries and the lives that exist within.', 'Brooklyn.mp4', 'November 25, 2015'),
+(13, 'walk.jpg', 'The Walk', '2015', '2h 3m', 'In 1974, high-wire artist Philippe Petit recruits a team of people to help him realize his dream: to walk the immense void between the World Trade Center towers.', 'Walk.mp4', 'October 9, 2015'),
+(14, 'colonia.jpg', 'Colonia', '2015', '1h 46m', 'A young woman&rsquo;s desperate search for her abducted boyfriend that draws her into the infamous Colonia Dignidad, a sect nobody has ever escaped from.', 'Colonia.mp4', 'April 15, 2015'),
+(15, 'force.jpg', 'Star Wars: The Force Awakens', '2015', '2h 16m', 'Three decades after the Empire&rsquo;s defeat, a new threat arises in the militant First Order. Stormtrooper defector Finn and spare parts scavenger Rey are caught up in the Resistance&rsquo;s search for the missing Luke Skywalker.', 'Force.mp4', 'December 18, 2015'),
+(16, 'whiplash.jpg', 'Whiplash', '2014', '1h 47m', 'A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student\'s potential.', 'Whiplash.mp4', 'October 15, 2014'),
+(17, 'guardians.jpg', 'Guardians of the Galaxy', '2014', '2h 1m', 'A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.', 'Guardians.mp4', 'August 1, 2014'),
+(18, 'donjon.jpg', 'Don Jon', '2013', '1h 30m', 'A New Jersey guy dedicated to his family, friends, and church, develops unrealistic expectations from watching porn and works to find happiness and intimacy with his potential true love.', 'donjon.mp4', 'September 27, 2013'),
+(19, 'gravity.jpg', 'Gravity', '2013', '1h 31m', 'Two astronauts work together to survive after an accident which leaves them alone in space.', 'Gravity.mp4', 'October 4, 2013'),
+(20, 'pi.jpg', 'Life of Pi', '2012', '2h 7m', 'A young man who survives a disaster at sea is hurtled into an epic journey of adventure and discovery. While cast away, he forms an unexpected connection with another survivor: a fearsome Bengal tiger.', 'Pi.jpg', 'November 21, 2012'),
+(21, 'silver.jpg', 'Silver Linings Playbook', '2012', '2h 2m', 'After a stint in a mental institution, former teacher Pat Solitano moves back in with his parents and tries to reconcile with his ex-wife. Things get more challenging when Pat meets Tiffany, a mysterious girl with problems of her own.', 'Silver.jpg', 'December 25, 2012');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_movies_social_media_links`
+--
+
+CREATE TABLE `tbl_movies_social_media_links` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `movies_id` mediumint(8) NOT NULL,
+  `facebook_link` text,
+  `twitter_link` text,
+  `instagram_link` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_movies_social_media_links`
+--
+
+INSERT INTO `tbl_movies_social_media_links` (`id`, `movies_id`, `facebook_link`, `twitter_link`, `instagram_link`) VALUES
+(1, 1, 'https://www.facebook.com/guardiansofthegalaxy', 'https://twitter.com/Guardians', 'https://www.instagram.com/guardiansofthegalaxy/?hl=en'),
+(2, 2, 'https://www.facebook.com/Dunkirkmovie', 'https://twitter.com/dunkirkmovie', 'https://www.instagram.com/dunkirkmovie/'),
+(3, 3, 'https://www.facebook.com/TheWolverineMovie/?brand_redir=393442714073793', 'https://twitter.com/wolverinemovie', NULL),
+(4, 4, 'https://www.facebook.com/OKJAnetflix', 'https://twitter.com/okjanetflix', NULL),
+(5, 5, 'https://www.facebook.com/DisneyBeautyAndTheBeast', 'https://twitter.com/beourguest', 'https://www.instagram.com/beautyandthebeast/'),
+(6, 6, 'https://www.facebook.com/HacksawRidge/?brand_redir=1316020801764836', 'https://twitter.com/hacksawridge', NULL),
+(7, 7, 'https://www.facebook.com/DeadpoolMovie', 'https://twitter.com/deadpoolmovie', NULL),
+(8, 8, 'https://www.facebook.com/ArrivalMovie/?brand_redir=1637956449848563', 'https://twitter.com/arrivalmovie', 'https://www.instagram.com/ArrivalMovie/'),
+(9, 9, 'https://www.facebook.com/EddieTheEagleMovie', 'https://twitter.com/eddieeaglemovie', NULL),
+(10, 10, 'https://www.facebook.com/DreamWorksTrolls/?brand_redir=1046178062071204', 'https://twitter.com/trolls', 'https://www.instagram.com/trolls/'),
+(11, 11, 'https://www.facebook.com/RevenantMovie', 'https://twitter.com/revenantmovie', NULL),
+(12, 12, 'https://www.facebook.com/BrooklynMovie', NULL, NULL),
+(13, 13, 'https://www.facebook.com/TheWalkMovie', 'https://twitter.com/thewalkmovie', NULL),
+(14, 14, NULL, NULL, NULL),
+(15, 15, 'https://www.facebook.com/StarWars', 'https://twitter.com/starwars', 'https://www.instagram.com/starwars/'),
+(16, 16, 'https://www.facebook.com/WhiplashMovie', NULL, NULL),
+(17, 17, 'https://www.facebook.com/guardiansofthegalaxy', NULL, NULL),
+(18, 18, 'https://www.facebook.com/DonJonMovie', 'https://twitter.com/donjonmovie', NULL),
+(19, 19, 'https://www.facebook.com/gravitymovie', NULL, NULL),
+(20, 20, 'https://www.facebook.com/LifeofPi', NULL, NULL),
+(21, 21, NULL, 'https://twitter.com/silver_linings', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mov_director`
+--
+
+CREATE TABLE `tbl_mov_director` (
+  `mov_director_id` mediumint(8) UNSIGNED NOT NULL,
+  `movies_id` mediumint(9) NOT NULL,
+  `director_id` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_mov_director`
+--
+
+INSERT INTO `tbl_mov_director` (`mov_director_id`, `movies_id`, `director_id`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4),
+(5, 5, 5),
+(6, 6, 6),
+(7, 7, 7),
+(8, 8, 8),
+(9, 9, 9),
+(10, 10, 10),
+(11, 10, 11),
+(12, 11, 12),
+(13, 12, 13),
+(14, 13, 14),
+(15, 14, 15),
+(16, 15, 16),
+(17, 16, 17),
+(18, 17, 1),
+(19, 18, 19),
+(20, 19, 20),
+(21, 20, 21);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mov_genre`
+--
+
+CREATE TABLE `tbl_mov_genre` (
+  `mov_genre_id` mediumint(8) UNSIGNED NOT NULL,
+  `movies_id` mediumint(9) NOT NULL,
+  `genre_id` mediumint(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_mov_genre`
+--
+
+INSERT INTO `tbl_mov_genre` (`mov_genre_id`, `movies_id`, `genre_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 9),
+(4, 2, 1),
+(5, 2, 5),
+(6, 2, 6),
+(7, 2, 10),
+(8, 3, 1),
+(9, 3, 5),
+(10, 3, 9),
+(11, 4, 1),
+(12, 4, 2),
+(13, 4, 5),
+(14, 4, 9),
+(15, 5, 8),
+(16, 5, 13),
+(17, 5, 14),
+(18, 6, 5),
+(19, 6, 6),
+(20, 6, 10),
+(21, 7, 1),
+(22, 7, 2),
+(23, 7, 3),
+(24, 7, 15),
+(25, 8, 5),
+(26, 8, 9),
+(27, 9, 3),
+(28, 9, 5),
+(29, 9, 16),
+(30, 10, 14),
+(31, 10, 2),
+(32, 10, 3),
+(33, 10, 8),
+(34, 10, 13),
+(35, 11, 2),
+(36, 11, 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_arating`
+--
+ALTER TABLE `tbl_arating`
+  ADD PRIMARY KEY (`arating_id`);
+
+--
+-- Indexes for table `tbl_comments`
+--
+ALTER TABLE `tbl_comments`
+  ADD PRIMARY KEY (`comments_id`);
+
+--
+-- Indexes for table `tbl_director`
+--
+ALTER TABLE `tbl_director`
+  ADD PRIMARY KEY (`director_id`);
+
+--
+-- Indexes for table `tbl_genre`
+--
+ALTER TABLE `tbl_genre`
+  ADD PRIMARY KEY (`genre_id`);
+
+--
+-- Indexes for table `tbl_movies`
+--
+ALTER TABLE `tbl_movies`
+  ADD PRIMARY KEY (`movies_id`);
+
+--
+-- Indexes for table `tbl_movies_social_media_links`
+--
+ALTER TABLE `tbl_movies_social_media_links`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `tbl_mov_director`
+--
+ALTER TABLE `tbl_mov_director`
+  ADD PRIMARY KEY (`mov_director_id`);
+
+--
+-- Indexes for table `tbl_mov_genre`
+--
+ALTER TABLE `tbl_mov_genre`
+  ADD PRIMARY KEY (`mov_genre_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_arating`
+--
+ALTER TABLE `tbl_arating`
+  MODIFY `arating_id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `tbl_comments`
+--
+ALTER TABLE `tbl_comments`
+  MODIFY `comments_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- AUTO_INCREMENT for table `tbl_director`
+--
+ALTER TABLE `tbl_director`
+  MODIFY `director_id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `tbl_genre`
+--
+ALTER TABLE `tbl_genre`
+  MODIFY `genre_id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `tbl_movies`
+--
+ALTER TABLE `tbl_movies`
+  MODIFY `movies_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `tbl_mov_director`
+--
+ALTER TABLE `tbl_mov_director`
+  MODIFY `mov_director_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `tbl_mov_genre`
+--
+ALTER TABLE `tbl_mov_genre`
+  MODIFY `mov_genre_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ routes/index.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var connect = require('../utils/sqlConnect');
+var router = express.Router();
+var aync = require('async');
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  // do an SQL query to get all of the movies, including genre
+  connect.query(`SELECT * FROM tbl_movies m, tbl_genre g, tbl_mov_genre mg WHERE m.movies_id = mg.movies_id AND g.genre_id = mg.genre_id`, (error, rows)=> {
+    if (error) {
+      throw error;
+    } else {
+      res.render('home', {
+        defaultMovie : rows[Math.floor(Math.random() * rows.length)],
+        data : JSON.stringify(rows),
+        mainpage : true,
+        videopage : false
+      });
+    }
+  })
+});
+
+router.get('/movies/:id/:genre_id/:vidsrc', (req, res) => {
+  console.log(req.params.id, req.params.vidsrc)
+  //Getting movies details and comments through id
+  connect.query(`SELECT * FROM tbl_comments c WHERE comments_movie = ${req.params.id}`, (err, rows)=> {
+    var movie_data;
+    if (err) {
+      console.log(err);
+    } 
+    else {
+      connect.query(`SELECT * FROM tbl_movies WHERE movies_id = ${req.params.id}`, (err, rows1)=> {
+        if (err) {
+          console.log(err);
+        } 
+        else {
+              movie_data = {
+              title : rows1[0].movies_title,
+              description : rows1[0].movies_storyline,
+              genre : rows1[0].movies_genre,
+              genre_id : req.params.genre_id
+          };
+          connect.query(`SELECT * FROM tbl_mov_genre WHERE genre_id = ${req.params.genre_id}`, (err, rows2)=> {
+            if (err) {
+              console.log(err);
+            } 
+            else {
+              var getmoviesname= [];
+              var getmoviesconver={};
+              for(var i=0; i<rows2.length; i++){
+                getmoviesname.push(rows2[i].movies_id);
+              }
+              var q1 = 'SELECT * FROM tbl_movies WHERE movies_id IN ('+getmoviesname+ ')';
+              connect.query(q1, (err, rows3)=> {
+                if (err) {
+                  console.log(err);
+                } 
+                else {
+                  rows3.genre_id = req.params.genre_id;
+                  
+                  console.log('genre id is : '+req.params.genre_id);
+                  res.render('movie', {
+                    movie : req.params.id,
+                    trailer : req.params.vidsrc,
+                    movie_data : movie_data,
+                    genre_id : req.params.genre_id,
+                    data : JSON.stringify(rows),
+                    data1 : rows3,
+                    mainpage : false,
+                    videopage: true
+                  }); 
+                }
+              }); //end of 4th query
+              
+            }
+          }); //end of 3rd query
+            
+        }
+      }); //end of 2nd query   
+    }
+  }); //end of 1st query
+
+});
+
+
+
+module.exports = router;
